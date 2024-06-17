7675267c525b12685e1258d0d746dc184095c782
adolfogutcampos <adolfogutcampos@gmail.com>
2024-06-17 15:35:00 +0200

    repo 4 7

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import sqlite3
 from flask import Flask, request, jsonify
 from sklearn.linear_model import LinearRegression
 
+os.chdir(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
